e32e22def61569f39e52eee48ef8748eb9de3a65
me@mrlee.dev <me@mrlee.dev>
2021-08-28 17:39:41 +0000

    Add new post: The Bookshelf

---
+++ posts/the-bookshelf.poly.pm
@@ -0,0 +1,56 @@
+#lang pollen
+
+◊define-meta[title]{The Bookshelf}
+◊define-meta[date]{2021-08-28}
+◊define-meta[published #t]
+◊define-meta[category]{personal}
+
+They say you can learn a lot about a person by the contents of their bookshelf. If you were to look at mine you'd see the beginnings of a hoarding mentality. Not unlike my Steam library, there are some books on my shelf that I will probably never read. But at least I can be pretentious and say I have them, so long as I can bullshit my way through any conversation about them. Yes, I'm talking about you, ◊em{Structure and Interpretation of Computer Programs}.
+
+There's quite an eclectic mix, all things considered, although many of the books are more useful as references rather than things I'd read cover-to-cover. I've got a few books about Complex PTSD--which is something about my mental health that I have to deal with--and a bunch of books relating to coaching, which has been one of my ways of dealing with it. The same can be said for the few books I have relating to Tantra, Sanskrit and spirituality in general.
+
+Scanning the shelves, I'm noticing a particular taste for weird horror: The King in Yellow is practically the birth of the genre and House of Leaves takes it to another level. Naturally, I also love Twin Peaks and the X-Files. Spooky.
+
+Programming books are almost disappointingly over-represented as they're possibly the most boring reads of the lot. But they're immensely practical, as I'm sure readers of Data Driven Design, Designing Data Intensive Applications, and Domain Modelling Made Functional can probably attest to. But there's another category of literature that belongs in a similar space; the one that Peopleware, The 5 Dysfunctions of a Team, and Mythical Man Month belong to. Unlike SICP, I actually did find the time to read those and not just buy them, and it's disappointing that nothing has really changed since the time when they were published.
+
+I added a few new books to the collection this year, and have been reading them as they arrive. Here's a quick run-through of what 2021 has looked like for me:
+
+
+◊h2{The Phoenix Project}◊^[1]
+
+As far as Corporate Thrillers for Business Analysts go, The Phoenix Project easy enough to read and has some entertainment value. The cast is awash with wholly unsympathetic characters, the corporate setting oozes with toxicity, and it's impossible to turn a page without ◊em{someone} being an asshole.
+
+I didn't rate it and if its follow up ◊em{The Unicorn Project} is any similar, then I think I'll give that one a pass. The Devops Handbook is still on my list, though.
+
+◊h2{Extreme Ownership}◊^[2]
+
+I had to keep an open mind when starting this one, but eventually I gave up because I could no longer ignore what I was really feeling while reading this. Extreme Ownership glorifies the war in Iraq and the US's role in it and uses those experiences to teach you about accountability. 
+
+The book mostly follows a pattern of telling a war story (each one accompanied by a considerable body count) and then relating that experience to corporate leadership. As if the death of a few dozen Iraqis is comparable to a VP of Finance owning up to their failures to run a project.
+
+Let's be fair, the advice is sound if you can put the chest beating and the hoo-rah to one side and focus purely on the advice. And the advice, taken outside of that context, ◊em{is} sound. But it's also shallow and, in essence, a vehicle for the authors to sell consulting and coaching services to business executives.
+
+This one just wasn't for me.
+
+◊h2{Metro 2033}◊^[3]
+
+Like many people, my first intro to Metro 2033 was the game that was released a decade ago, and I've greatly enjoyed Artyom's journey from then up until the release of Metro Exodus. The post-apocalyptic setting is remarkable and I was sucked into the world in a way that Fallout could never manage. The stories feel more grounded in humanity and they're not quite as distracted by commentary about the excesses of capitalism.
+
+I decided to revisit the world again this year, except this time by reading the books. I haven't finished this yet, as I've wanted to take my time with it. 
+
+◊h2{Crucial Conversations}◊^[4]
+
+I revisit this book every now and then, although it has the dual purpose of selling consultation along with offering a taste of advice. Once you get used to the relentless sales pitch for Crucial Conversations™ you can actually take away a great deal of learning from this.
+
+It's the kind of book I'm happy to lend to someone without an expectation of them giving it back when they're done, so I've had a few different copies over the past several years. And it's one of the handful of books I've read cover-to-cover that has materially changed how I approach communication.
+
+◊hr{}
+
+Beyond these, I have both Dune and Systemic Coaching & Constellations on the backburner. I'm looking forward to getting stuck into both.
+
+◊footnotes{
+  ◊^[1]{◊<>["https://uk.bookshop.org/a/6865/9781942788294"]}
+  ◊^[2]{◊<>["https://uk.bookshop.org/a/6865/9781250183866"]}
+  ◊^[3]{◊<>["https://uk.bookshop.org/a/6865/9780575086258"]}
+  ◊^[4]{◊<>["https://uk.bookshop.org/a/6865/9780071771320"]}
+}
