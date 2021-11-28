ee79560e96ab5e95a071491752635a7c43da21a8
me@mrlee.dev <me@mrlee.dev>
2021-11-28 16:22:07 +0000

    Pull deps from debian

--- publish.el
+++ publish.el
@@ -1,21 +1,5 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-				    'silent
-				    'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(defalias 'sup 'straight-use-package)
-
-(sup 'org)
-(sup 'htmlize)
-
+(require 'org)
+(require 'htmlize)
 (require 'ox-publish)
 
 (defun expand-relative-path (path)
